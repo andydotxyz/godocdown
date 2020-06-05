703f32130d3eda58674e70654abbefd815557eb9
Andy Williams <andy@andy.xyz>
2020-06-05 22:22:33 +0100

    Add code block for import statements
    and fix horizontal rule markdown

--- godocdown/render.go
+++ godocdown/render.go
@@ -52,13 +52,13 @@ func renderTypeListTo(writer io.Writer, list []*doc.Type) {
 }
 
 func renderHeaderTo(writer io.Writer, document *_document) {
-	fmt.Fprintf(writer, "# %s\n--\n", document.Name)
+	fmt.Fprintf(writer, "# %s\n---\n", document.Name)
 
 	if !document.IsCommand {
 		// Import
 		if RenderStyle.IncludeImport {
 			if document.ImportPath != "" {
-				fmt.Fprintf(writer, spacer(4)+"import \"%s\"\n\n", document.ImportPath)
+				fmt.Fprintf(writer, indentCode("import \"" + document.ImportPath + "\"") + "\n\n")
 			}
 		}
 	}
@@ -88,6 +88,6 @@ func renderUsageTo(writer io.Writer, document *_document) {
 
 func renderSignatureTo(writer io.Writer) {
 	if RenderStyle.IncludeSignature {
-		fmt.Fprintf(writer, "\n\n--\n**godocdown** http://github.com/robertkrimen/godocdown\n")
+		fmt.Fprintf(writer, "\n\n---\n**godocdown** http://github.com/robertkrimen/godocdown\n")
 	}
 }
